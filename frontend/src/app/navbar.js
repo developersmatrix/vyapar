9452cc42270173cf6cdea0e50483ad91a5321747
Thimmaiah <thimaiahmp72@gmail.com>
2024-03-26 14:07:31 +0530

    first commit

---
+++ frontend/src/app/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="">
+            sdsdsd
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import navbar from "./navbar";
+import Navbar from "./navbar";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+      <Navbar />
+
+
+
+    </main>
+  );
+}
